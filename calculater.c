6c94e8dd3ca65bfcff8c78ec6de7bfe8fda9904e
Dinithi Epitakaduwa <dinithiepi@gmail.com>
2024-11-22 22:55:40 +0530

    uploading base code

---
+++ calculater.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    float a;
+    float b;
+    printf("entr first number:\n");
+    scanf("%f", &a);
+    printf("entr second number\n");
+    scanf("%f", &b);
+    int c;
+    printf("entr operator (1 for '+',2 for '-',3 for '*',4 for '/')\n");
+    scanf("%d", &c);
+    
+    
+    if (c==1)
+    {
+        printf("ans is %f\n",a+b);
+    }
+    if (c==2)
+    {
+        printf("ans is %f\n",a-b);
+    }
+    if (c==3)
+    {
+        printf("ans is %f\n",a*b);
+    }
+    if (c==4)
+    {
+        if (b==0)
+            printf("error");
+        else
+            printf("ans is %f\n",a/b);
+    }    
+    getch();
+}
